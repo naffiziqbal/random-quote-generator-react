2c731c1efdc2ae310bf200b9a53ef66442039464
naffiziqbal <f.nafiziqbal@gmail.com>
2022-09-17 12:09:34 +0600

    Feature

---
+++ script.js
@@ -0,0 +1,26 @@
+const loadData =async (quote_id)=> {
+    const url = `https://breakingbadapi.com/api/quotes/${quote_id}`;
+    const res = await fetch(url)
+    const data = await res.json();
+    displayData(data[0])
+}
+
+const displayData = (id)=>{
+    const dataContainer = document.getElementById('display-data');
+    const showData = document.createElement('div');
+    showData.innerHTML = `
+    <h4>${id?.quote ? id.quote : 'Try Refreshing Again'}</h4>
+    `;
+    dataContainer.appendChild(showData)
+}
+
+function randomID(){
+ let randomNum =    parseInt(Math.random() * 100);
+ if(randomNum > 30 &&randomNum < 63){
+ randomNum = randomNum  /  3 ;
+ randomNum = parseInt(randomNum)
+ }
+//  console.log(randomNum)
+ return randomNum
+}
+loadData(randomID())
